92931b54b2a3f642739abc0dd65955ebfe1ade81
PDyson1400 <pdyson1400@gmail.com>
2023-02-21 14:14:47 +0000

    initial codebase

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-const UserController = require("./Controllers/UserController")
+const ListenerController = require("./Controllers/ListenerController")
 var bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
@@ -11,32 +11,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', async function (req, res) {
-    let cont = new UserController()
-    cont.getAll().then((object) => {
-        res.render('pages/index', {variable: object});
-    })
-})
-
-app.get('/sign_up/listener', async function (req, res) {
-    res.render('pages/sign_up_listener')
-})
-
-app.post('/log_in/listener', async function (req, res) {
-    let user = new UserController();
-    user.sign_up(req.body.first_name, req.body.last_name, req.body.email, req.body.password, "Listener")
-
-    //res.render('pages/log_in');
-})
-
-app.get('/sign_up/promoter', async function (req, res) {
-    res.render('pages/sign_up_promoter')
-})
-
-app.post('/log_in/promoter', async function (req, res) {
-    let user = new UserController();
-    user.sign_up_p(req.body.first_name, req.body.last_name, req.body.email, req.body.password, "Promoter", req.body.company_name, req.body.location)
-
-    //res.render('pages/log_in');
+    res.render('pages/index');
 })
 
 app.listen(3000);
