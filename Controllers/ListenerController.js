27edfdc49c7f571837f694d26703269e07c498a7
PDyson1400 <pdyson1400@gmail.com>
2023-02-21 15:55:08 +0000

    sign_up

--- Controllers/ListenerController.js
+++ Controllers/ListenerController.js
@@ -15,6 +15,23 @@ class ListenerController {
         
         }
     }
+
+    async sign_up(first_name, last_name, email, password) {
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO listeners (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)', [first_name, last_name, email, password])
+        client.end();
+    }
+
+    async log_in(email, password) {
+        const client = await pool.connect();
+        try {
+            const result = await client.query('SELECT * FROM listeners WHERE email = $1 AND password = $2', [email, password]);
+            return result.rows;
+        }
+        catch(err) {
+            console.log(err)
+        }
+    }
 }
 
 module.exports = ListenerController;
--- server.js
+++ server.js
@@ -14,4 +14,32 @@ app.get('/', async function (req, res) {
     res.render('pages/index');
 })
 
+app.post('/sign_up/success', async function (req, res) {
+    const listener = new ListenerController()
+    await listener.sign_up(req.body.first_name, req.body.last_name, req.body.email, req.body.password)
+    res.redirect('/log_in')
+})
+
+app.get('/log_in', async function (req, res) {
+    res.render('pages/log_in')
+})
+
+app.post('/user', async function (req, res) {
+    if (req.body.user_type === "listener") {
+        const listener = new ListenerController()
+        const result = await listener.log_in(req.body.email, req.body.password)
+        if (result.length !== 0) {
+            res.redirect(`/user/${result[0].id}`)
+        } else {
+            res.redirect('/log_in');
+        }
+    } else if (req.body.user_type === "promoter") {
+
+    };
+})
+
+app.get('/user/:id', async function (req, res) {
+    res.render('/pages/user_page')
+});
+
 app.listen(3000);
